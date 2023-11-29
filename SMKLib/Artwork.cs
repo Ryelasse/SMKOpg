f8f2e79b2e640c218608d4503392573c91d5dc9f
boeger01 <matias.boeger@gmail.com>
2023-11-29 10:43:10 +0100

    Artwork created

--- SMKLib/Artwork.cs
+++ SMKLib/Artwork.cs
@@ -2,6 +2,19 @@
 {
     public class Artwork
     {
+        public Artwork() 
+        {
+        
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double MinTemp { get; set; }
+        public double MaxTemp { get; set; }
+        public double MinHumiditty { get; set; }
+        public double MaxHumiditty { get; set; }
+        
+                                        
 
     }
 }
