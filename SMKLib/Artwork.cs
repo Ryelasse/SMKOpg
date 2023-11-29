d29bf7a341f842c5d9f8b19a83ca149df1309800
boeger01 <matias.boeger@gmail.com>
2023-11-29 10:50:11 +0100

    artwork

--- SMKLib/Artwork.cs
+++ SMKLib/Artwork.cs
@@ -2,8 +2,14 @@
 {
     public class Artwork
     {
-        public Artwork() 
+        public Artwork(int id, string name, double mintemp, double maxtemp, double minhumidity, double maxhumidity) 
         {
+            Id = id;
+            Name = name;
+            MinTemp = mintemp;
+            MaxTemp = maxtemp;
+            MinHumidity = minhumidity;
+            MaxHumidity = maxhumidity;
         
         }
 
@@ -11,8 +17,21 @@
         public string Name { get; set; }
         public double MinTemp { get; set; }
         public double MaxTemp { get; set; }
-        public double MinHumiditty { get; set; }
-        public double MaxHumiditty { get; set; }
+        public double MinHumidity { get; set; }
+        public double MaxHumidity { get; set; }
+
+        public void ValidateMinTemp()
+        {
+            if(MinTemp < 0) 
+            {
+                throw new ArgumentOutOfRangeException(nameof(MinTemp), $"");
+            }
+            if(MaxTemp < 0) 
+            {
+                throw new ArgumentOutOfRangeException(nameof(MaxTemp), $"");
+            }
+
+        }
         
                                         
 
