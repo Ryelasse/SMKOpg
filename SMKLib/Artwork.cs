b1194069c3853bdde73fbdd7f20104d147863c92
Nicklas Peters Pedersen <nick7845@edu.zealand.dk>
2023-11-29 11:00:18 +0100

    Artwork class added methods

--- SMKLib/Artwork.cs
+++ SMKLib/Artwork.cs
@@ -2,9 +2,9 @@
 {
     public class Artwork
     {
-        public Artwork(int id, string name, double mintemp, double maxtemp, double minhumidity, double maxhumidity) 
+        public Artwork(int artid, string name, double mintemp, double maxtemp, double minhumidity, double maxhumidity) 
         {
-            Id = id;
+            ArtId = artid;
             Name = name;
             MinTemp = mintemp;
             MaxTemp = maxtemp;
@@ -13,27 +13,44 @@
         
         }
 
-        public int Id { get; set; }
+        public int ArtId { get; set; }
         public string Name { get; set; }
         public double MinTemp { get; set; }
         public double MaxTemp { get; set; }
         public double MinHumidity { get; set; }
         public double MaxHumidity { get; set; }
 
-        public void ValidateMinTemp()
+        public void ValidateTemp()
         {
             if(MinTemp < 0) 
             {
-                throw new ArgumentOutOfRangeException(nameof(MinTemp), $"");
+                throw new ArgumentOutOfRangeException(nameof(MinTemp), $"The Temperature is too low!");
             }
-            if(MaxTemp < 0) 
+            if(MaxTemp > 0) 
             {
-                throw new ArgumentOutOfRangeException(nameof(MaxTemp), $"");
+                throw new ArgumentOutOfRangeException(nameof(MaxTemp), $"The Temperature is too high!");
             }
 
         }
-        
-                                        
+
+        public void ValidateHumidity()
+        {
+            if (MinHumidity < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(MinHumidity), $"The Humidity is too low!");
+            }
+            if ( MaxHumidity > 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(MaxHumidity), $"The Humidity is too high!");
+            }
+        }
+
+        public void Validate()
+        {
+            ValidateHumidity();
+            ValidateTemp();
+        }
+                        
 
     }
 }
